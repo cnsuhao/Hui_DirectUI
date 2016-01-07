7fe7a610dfa9874bd593be9b70459d2f07e91f47
wang70937 <wang70937@8f249d82-f7ca-4dc2-9878-2458fd577cf8>
2016-01-07 10:14:09 +0000

    增加ButtonControl消息响应

---
+++ DirectUI_Project/DirectUICom/_ITextStylePropEvents_CP.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template <class T>
+class CProxy_ITextStylePropEvents : public IConnectionPointImpl<T, &__uuidof( _ITextStylePropEvents ), CComDynamicUnkArray>
+{
+	//警告此类将由向导重新生成。
+public:
+};
